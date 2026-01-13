c1af103bfb3681e5bf9f5c4ff3e631399db4c625
quinlinfournier <quinlin.fournier@uvm.edu>
2026-01-13 16:58:31 -0500

    Added Simulation Steps

--- simulate.py
+++ simulate.py
@@ -1,4 +1,10 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+for i in range(1000):
+    print("Simulation step:", i)
+    time.sleep(1/60)
+    p.stepSimulation()
 p.disconnect()
